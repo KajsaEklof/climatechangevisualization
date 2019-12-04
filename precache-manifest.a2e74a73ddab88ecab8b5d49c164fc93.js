a6ee5f291d694ad4221cd4a8117198fa1ca9d1a8
Kajsa1 <kajsaKeklof@gmail.com>
2019-12-04 07:08:21 +0000

    Updates

---
+++ precache-manifest.a2e74a73ddab88ecab8b5d49c164fc93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab660338fd38291154136540c554edcb",
+    "url": "/climatechangevisualization/index.html"
+  },
+  {
+    "revision": "a2bb8f560b6993dd5688",
+    "url": "/climatechangevisualization/static/css/2.247797c6.chunk.css"
+  },
+  {
+    "revision": "3eec32637887df32461e",
+    "url": "/climatechangevisualization/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a2bb8f560b6993dd5688",
+    "url": "/climatechangevisualization/static/js/2.ac3288b2.chunk.js"
+  },
+  {
+    "revision": "3eec32637887df32461e",
+    "url": "/climatechangevisualization/static/js/main.04293c44.chunk.js"
+  },
+  {
+    "revision": "5ef1876d724baaad3b43",
+    "url": "/climatechangevisualization/static/js/runtime-main.e94b442e.js"
+  }
+]);
